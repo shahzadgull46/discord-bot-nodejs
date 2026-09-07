8654a0505de0302ae7fefbaff291659944ef5445
Shahzad Gull <shahzadgull46@gmail.com>
2026-09-07 06:58:26 +0500

    Prepare Discord bot for deployment

--- command.js
+++ command.js
@@ -1,36 +1,35 @@
 const { REST, Routes } = require("discord.js");
 require("dotenv").config();
 const commands = [
-    {
-        name: "ping",
-        description: "Replies with Pong!",
-    },
-    {
-        name: "create",
-        description: "Create a short URL",
-        options: [
-            {
-                name: "url",
-                description: "Enter the URL you want to shorten",
-                type: 3,
-                required: true,
-            },
-        ],
-    },
+  {
+    name: "ping",
+    description: "Replies with Pong!",
+  },
+  {
+    name: "create",
+    description: "Create a short URL",
+    options: [
+      {
+        name: "url",
+        description: "Enter the URL you want to shorten",
+        type: 3,
+        required: true,
+      },
+    ],
+  },
 ];
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
-    try {
-        console.log("Started refreshing application (/) commands.");
+  try {
+    console.log("Started refreshing application (/) commands.");
 
-        await rest.put(
-            Routes.applicationCommands("1544011825555968000"),
-            { body: commands }
-        );
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+      body: commands,
+    });
 
-        console.log("Successfully reloaded application (/) commands.");
-    } catch (error) {
-        console.error(error);
-    }
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
 })();
